798b31567c6c9981e0b5763c3a9ba4ad791154cf
Bharath Leon Sam <leonsam7477@gmail.com>
2026-08-28 13:59:45 -0400

    Add Python data type fundamentals

--- hello_genai.py
+++ hello_genai.py
@@ -1,2 +1,14 @@
-message = "Hello, Enterprise Gen AI!"
-print(message)
+project_name = "Enterprise GenAI Lab"
+day_number = 1
+is_learning = True
+print(project_name)
+print(day_number)
+print(is_learning)
+print(type(project_name))
+print(type(day_number))
+print(type(is_learning))
+next_day = day_number + 1
+print(next_day)
+temperature = 0.7
+print(temperature)
+print(type(temperature))
